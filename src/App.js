8f540daeebfda090cc87a79f13506041d75c9be0
Titan <jamisontitan@gmail.com>
2019-01-20 19:18:11 -0800

    got sign in working on herkoku

--- src/App.js
+++ src/App.js
@@ -4,12 +4,50 @@ import queryString from 'query-string';
 
 let styleOne = {color:'#909', 'fontSize': '24px'};
 let styleTwo = {color:'#990', 'fontSize': '20px'};
-let btnStyle = {padding: '20px', 'font-size': '54px', 'marginTop':'20px'}
+let btnStyle = {padding: '20px', 'fontSize': '54px', 'marginTop':'20px'}
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div style={{...styleOne, width: "40%", display: 'inline-block'}}>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+      <div style={{...styleOne, width: "40%", display: 'inline-block'}}>
+        <h2>{Math.round(totalDuration/60)} hours</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={{...styleOne, padding : '12px'}}>
+        <img/>
+        <input type="text" onKeyUp={event =>
+          this.props.onTextChange(event.target.value)}/>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { serverData: {}};
+    this.state = { filterString: ''};
   }
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
@@ -21,25 +59,59 @@ class App extends Component {
         headers: {'authorization': `Bearer ${accessToken}`}
       }).then(response => response.json())
       .then(data => {
-        console.log(data.display_name)
-        this.setState({serverData: {user: {name: data.display_name}}})
+        console.log(data)
+        this.setState({
+            user: {
+                name: data.display_name,
+              }
+        });
       });
+
+      fetch('https://api.spotify.com/v1/me/playlists',{
+        headers: {'authorization': `Bearer ${accessToken}`}
+      }).then(response => response.json())
+      .then(data => {
+          this.setState({
+            playlists: data.items.map(item => {
+              console.log(item)
+              return {
+                name: item.name,
+                imageURL: item.images[0].url,
+                songs: []
+              }
+            }),
+          });
+      });
+
   }
+
+
   render() {
+    let playlistToRender =
+       this.state.user &&
+       this.state.playlists
+         ? this.state.playlists.filter(playlist =>
+           playlist.name.toLowerCase().includes(
+             this.state.filterString.toLowerCase()))
+ : []
     return (
       <div className="App">
-        { this.state.serverData.user ?
+        { this.state.user ?
         <div>
-        <h1 style={{...styleOne, 'font-size': '48px'}}>
-          {this.state.serverData.user &&
-          this.state.serverData.user.name}s Playlist
+        <h1 style={{...styleOne, 'fontSize': '48px'}}>
+          {this.state.user &&
+          this.state.user.name}s Playlists
         </h1>
-        <Aggregate songs={this.state.serverData.user &&
-                          this.state.serverData.user.songs}/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-      </div> : <button onClick={() =>  window.location='http://localhost:8888/login'}
+        <Filter onTextChange={text => {
+            this.setState({filterString: text})
+          }}/>
+      {playlistToRender.map(playlist =>
+        <Playlist playlist={playlist} />
+      )}
+      </div> : <button onClick={() => {
+        window.location = window.location.href.includes('localhost')
+        ? 'http://localhost:8888/login'
+        : 'https://naive-react-app-backend.herokuapp.com/'}}
       style={btnStyle}>Sign in with spotify</button>
     }
     </div>
@@ -47,37 +119,18 @@ class App extends Component {
   }
 }
 
-class Aggregate extends Component {
-  render() {
-    return (
-      <div style={{...styleOne, width:'40%', display:'inline-block'}}>
-        <h1> song length {this.props.songs &&
-                          this.props.songs.length}</h1>
-      </div>
-    );
-  }
-}
-
-class Filter extends Component {
-  render() {
-    return (
-      <div>
-        <input type='text'/>
-      </div>
-    );
-  }
-}
-
 class Playlist extends Component {
   render() {
-    return(
-      <div style={{...styleTwo, width: '25%'}}>
-        <h3 >playlist Name</h3>
+    let playlist = this.props.playlist
+    return (
+      <div style={{...styleOne, display: 'inline-block', width: "25%"}}>
+        <img src={playlist.imageURL} style={{width: '60px', height: '60px', padding: '12px'}}/>
+        <h3>{playlist.name}</h3>
         <ul>
-          <li> song 1</li>
-          <li> song 2 </li>
-          <li> song 3 </li>
-        </ul>d r
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
