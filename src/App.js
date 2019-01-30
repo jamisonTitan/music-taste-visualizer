5d7649d9b1a363b252ecaf06bc41022852f9cb67
Titan <jamisontitan@gmail.com>
2019-01-29 18:15:04 -0800

    finally got all the data fetched from spotify api working on aesthetics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import queryString from 'query-string';
+import { Line, Circle } from 'rc-progress';
 
 let styleOne = {color:'#303', 'fontSize': '24px'};
 let styleTwo = {color:'#330', 'fontSize': '20px'};
@@ -16,13 +17,68 @@ class PlaylistCounter extends Component {
   }
 }
 
+class ListeningData extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    let playlistsAudioData = []
+    let playlists = this.props.playlists;
+    let averages = {
+          danceability: 0,
+          energy: 0,
+          loudness: 0,
+          acousticness: 0,
+          instrumentalness: 0,
+          valence : 0,
+          tempo : 0,
+        };
+    playlists.forEach((playlist, i) => {
+      let keys;
+       playlist.forEach((songData, j) => {
+         if(songData){
+           keys = Object.keys(songData);
+           keys.forEach(key => {
+             averages[key] += songData[key]
+           });
+        }
+       })
+       keys.forEach(key => {
+          //get average of values
+         averages[key] /= playlist.length;
+       });
+     });
+    return (
+      <div style={{width:'50%',padding:'24px'}}>
+        <h2>danceability:{Math.floor(averages.danceability * 100)}</h2>
+        <Line percent={Math.floor(averages.danceability * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="#800080"/>
+        <h2>Energy:{Math.floor(averages.energy * 100)}</h2>
+        <Line percent={Math.floor(averages.energy * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(220,20,60)"/>
+        <h2>Loudness:{Math.floor((averages.loudness + 60) * (100/60))}</h2>
+        <Line percent={Math.floor((averages.loudness + 60) * (100/60))} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="#000"/>
+        <h2>Acousticness:{Math.floor(averages.acousticness * 100)}</h2>
+        <Line percent={Math.floor(averages.acousticness * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(255,228,181)"/>
+        <h2>Valence:{Math.floor(averages.valence * 100)}</h2>
+        <Line percent={Math.floor(averages.valence * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(50,205,50)"/>
+      </div>
+    )
+  }
+}
+
 class HoursCounter extends Component {
   render() {
     let songsToDisplay = this.props.playlists.reduce((songsToDisplay, playlists) => {
       return songsToDisplay.concat(playlists.songsToDisplay)
     }, [])
     let totalDuration = songsToDisplay.reduce((sum, song) => {
-      console.log(song.duration);
       return sum + song.duration
     }, 0)
     return (
@@ -51,30 +107,7 @@ class App extends Component {
     this.state = { filterString: ''};
   }
   componentDidMount() {
-    let playlists;
-    let parsed = queryString.parse(window.location.search);
-      let accessToken = parsed.access_token;
-      if (!accessToken)
-      return;
-      console.log(accessToken);
-      fetch('https://api.spotify.com/v1/me',{
-        headers: {'authorization': `Bearer ${accessToken}`}
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        this.setState({
-            user: {
-                name: data.display_name,
-              }
-        });
-      });
-
-      fetch('https://api.spotify.com/v1/me/playlists', {
-        headers: {'Authorization': 'Bearer ' + accessToken}
-      }).then(response => response.json())
-      .then(playlistData => {
-        console.log(playlistData.items);
+    let getPlaylistData = playlistData => {
         playlists = playlistData.items;
         let trackDataPromises = playlists.map(playlist => {
           let responsePromise = fetch(playlist.tracks.href, {
@@ -86,33 +119,90 @@ class App extends Component {
         })
         let allTracksDataPromises =
           Promise.all(trackDataPromises)
-        let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+          let playlistsPromise = allTracksDataPromises.then(trackDatas => {
           trackDatas.forEach((trackData, i) => {
-            playlists[i].trackDatas = trackData.items
-              .map(item => item.track)
-              .map(trackData => ({
-                name: trackData.name,
-                duration: trackData.duration_ms / 1000,
-                uri: trackData.uri.split(':')[2]
-              }))
+            if(playlists[i]){
+              playlists[i].trackDatas = trackData.items
+                .map(item => item.track)
+                .map(trackData => ({
+                  name: trackData.name,
+                  duration: trackData.duration_ms / 1000,
+                  uri: trackData.uri.split(':')[2]
+                }))
+            }
           })
           return playlists;
+
         })
         return playlistsPromise;
-  })
-      .then(playlists => {
-          this.setState({
-            playlists: playlists.map(item => {
-              return {
-                  name: item.name,
-                  imageURL: item.images[0].url,
-                  songsToDisplay: item.trackDatas.slice(0,3),
-                  songs: item.trackDatas
+  }
+
+    let playlists;
+    let parsed = queryString.parse(window.location.search);
+      let accessToken = parsed.access_token;
+      if (!accessToken)
+      return;
+      fetch('https://api.spotify.com/v1/me',{
+        headers: {'authorization': `Bearer ${accessToken}`}
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+            user: {
+                name: data.display_name,
               }
-            }),
-          });
+        });
       });
-      console.log(playlists);
+      let audioDatas = [];
+      fetch('https://api.spotify.com/v1/me/playlists', {
+        headers: {'Authorization': 'Bearer ' + accessToken}
+      }).then(response => response.json())
+      .then(getPlaylistData)
+      .then(playlists => {
+          let songUriArrays = playlists.map(item => {
+            return item.trackDatas.map(trackData => {return trackData.uri;})
+          })
+          let allSongUriPromises = Promise.all(songUriArrays.map(songUris => {
+              let songDatasPromise = fetch(`https://api.spotify.com/v1/audio-features/?ids=${
+                songUris.slice(0,100).join(',') //only first 100 songs
+              }`,{
+                headers: {'authorization': `Bearer ${accessToken}`}
+              }).then(res => res.json())
+              return songDatasPromise
+            })
+          )
+          return allSongUriPromises;
+        })
+        .then(audioDatas => {
+          let audioFeatures = audioDatas.map(audioData => {
+            let audioFeatures = audioData.audio_features.map(item => {
+              if(item){
+               return {
+                 danceability: item.danceability,
+                 energy: item.energy,
+                 loudness:item.loudness,
+                 acousticness: item.acousticness,
+                 instrumentalness: item.instrumentalness,
+                 valence : item.valence,
+                 tempo : item.tempo,
+               }
+              }
+             })
+             return audioFeatures;
+          })
+            this.setState({
+              playlists: playlists.map((item, i) => {
+                return {
+                    name: item.name,
+                    imageURL: item.images[0].url,
+                    songsToDisplay: item.trackDatas.slice(0,3),
+                    songs: item.trackDatas,
+                    audioFeatures: audioFeatures[i]
+                }
+
+              }),
+            });
+          })
 
   }
 
@@ -138,8 +228,14 @@ class App extends Component {
           }}/>
       <PlaylistCounter playlists={playlistsToRender}/>
       <HoursCounter playlists={playlistsToRender}/>
-      {playlistsToRender.map(playlist =>
-        <Playlist playlist={playlist} />
+      <ListeningData playlists={
+        playlistsToRender.map(playlist=>{return playlist.audioFeatures
+      })}/>
+      {playlistsToRender.map((playlist, i) =>{
+        return(
+        <Playlist playlist={playlist} audioFeatures={playlist.audioFeatures} />
+      )
+      }
       )}
       </div> : <button onClick={() => {
         window.location = window.location.href.includes('localhost')
@@ -154,51 +250,75 @@ class App extends Component {
 }
 
 class Playlist extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-        songAudioData: {
-              danceability: 0,
-              acousticness: 0,
-              energy: 0,
-              instrumentalness: 0,
-              loudness: 0,
-              valence: 0,
-              bpm: 0
-            },
-      };
+      averages: {
+        danceability: 0,
+        energy: 0,
+        loudness: 0,
+        acousticness: 0,
+        instrumentalness: 0,
+        valence : 0,
+        tempo : 0,
+      },
+    };
   }
-
+  componentWillMount(){
+  //  console.log(this.props.playlists);
+    //console.log(this.props.index);
+    let audioFeatures = this.props.audioFeatures;
+    console.log(audioFeatures);
+    let keys;
+    let averages = {
+          danceability: 0,
+          energy: 0,
+          loudness: 0,
+          acousticness: 0,
+          instrumentalness: 0,
+          valence : 0,
+          tempo : 0,
+    }
+    audioFeatures.forEach((songData) => {
+      if(songData){
+        keys = Object.keys(songData);
+        keys.forEach(key => {
+          console.log(key, averages[key],songData[key])
+          averages[key] += songData[key]
+        });
+     }
+   });
+   keys.forEach(key => {
+        //get average of values
+       averages[key] /= audioFeatures.length;
+     });
+   this.setState({
+     averages: averages,
+   })
+  }
   render() {
-    let parsed = queryString.parse(window.location.search);
-    let accessToken = parsed.access_token;
     let playlist = this.props.playlist;
-    let length = 7;
-    playlist.songs.forEach((song, i) => {
-      fetch(`https://api.spotify.com/v1/audio-features/${playlist.songs[i].uri}`,{
-        headers: {'authorization': `Bearer ${accessToken}`}
-      })
-      .then(response => response.json())
-      .then(data => {
-      this.state.songAudioData.danceability += data.danceability;
-      this.state.songAudioData.acousticness += data.acousticness;
-      this.state.songAudioData.energy += data.energy;
-      this.state.songAudioData.instrumentalness += data.instrumentalness;
-      this.state.songAudioData.loudness += data.loudness;
-      this.state.songAudioData.valence += data.valence;
-      this.state.songAudioData.bpm +=  data.tempo;
-      });
-    });
-
     return (
       <div style={{...styleOne, display: 'inline-block',
       width: "100%", display: 'flex', 'justifyContent':'left'}}>
-        <div>
+        <h2>{playlist.name}</h2>
         <img src={playlist.imageURL} style={{width: '120px', height: '120px', padding: '12px'}}/>
-        <h3>{playlist.name}</h3>
-        </div>
+        <ul style={{width:'50%'}}>
+        <li> {Math.floor(this.state.averages.danceability * 100)}</li>
+        <li><Line percent={Math.floor(this.state.averages.danceability * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="#800080"/>{Math.floor(this.state.averages.danceability * 100)}</li>
+        <li><Line percent={Math.floor(this.state.averages.energy * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(220,20,60)"/>{Math.floor(this.state.averages.energy * 100)}</li>
+        <li><Line percent={Math.floor((this.state.averages.loudness + 60) * (100/60))} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="#000"/>{Math.floor((this.state.averages.loudness + 60) * (100/60))}</li>
+        <li><Line percent={Math.floor(this.state.averages.acousticness * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(255,228,181)"/>{this.state.averages.acousticness * 100}</li>
+        <li><Line percent={Math.floor(this.state.averages.valence * 100)} strokeWidth="4" trailWidth='4'
+        trailColor='#b3b3b3' strokeColor="rgb(50,205,50)"/>{Math.floor(this.state.averages.valence * 100)}</li>
+        </ul>
         <ul>
-          {playlist.songsToDisplay.map(song =>
+
+        {playlist.songsToDisplay.map(song =>
             <li>{song.name}</li>
           )}
         </ul>
